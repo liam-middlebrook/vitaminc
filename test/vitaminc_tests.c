2c1625363540d8b176937aa00e84cb0530ca7ec6
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-12-27 18:58:33 -0500

    added printlns to complementary color test

--- test/vitaminc_tests.c
+++ test/vitaminc_tests.c
@@ -1,4 +1,5 @@
 #include "vitaminc_tests.h"
+#include <stdio.h>
 
 int init_color_tests(void){
     return 0;
@@ -11,9 +12,12 @@ int clean_color_tests(void){
 void test_complementary_color(void){
     vc_color red = vc_create_color(1.0, 0.0, 0.0, VC_RGB, 1.0);
 
+    printf("Red: %d %d %d %d", red.r, red.g, red.b, red.a);
     vc_color yellow = vc_complementary_color(red, VC_WHEEL_RYB);
-
-    CU_ASSERT(vc_color_equals(red, vc_complementary_color(yellow, VC_WHEEL_RYB)));
+    printf("Yellow: %d %d %d %d", red2.g.r, yellow.g, yellow.b, yellow.a);
+    vc_color red2 = vc_complementary_color(yellow, VC_WHEEL_RYB);
+    printf("Red2: %d %d %d %d", red2.r, red2.g, red2.b, red2.a);    
+    CU_ASSERT(vc_color_equals(red, red2));
 }
 
 int main(){
