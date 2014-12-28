5c79277e59dac5e16d355009f840753e8c9dd1d3
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-12-27 19:03:33 -0500

    Updated format specifier for double

--- test/vitaminc_tests.c
+++ test/vitaminc_tests.c
@@ -12,11 +12,11 @@ int clean_color_tests(void){
 void test_complementary_color(void){
     vc_color red = vc_create_color(1.0, 0.0, 0.0, VC_RGB, 1.0);
 
-    printf("Red: %d %d %d %d", red.r, red.g, red.b, red.a);
+    printf("Red: %f %f %f %f", red.r, red.g, red.b, red.a);
     vc_color yellow = vc_complementary_color(red, VC_WHEEL_RYB);
-    printf("Yellow: %d %d %d %d", red2.g.r, yellow.g, yellow.b, yellow.a);
+    printf("Yellow: %f %f %f %f", red2.g.r, yellow.g, yellow.b, yellow.a);
     vc_color red2 = vc_complementary_color(yellow, VC_WHEEL_RYB);
-    printf("Red2: %d %d %d %d", red2.r, red2.g, red2.b, red2.a);    
+    printf("Red2: %f %f %f %f", red2.r, red2.g, red2.b, red2.a);    
     CU_ASSERT(vc_color_equals(red, red2));
 }
 
