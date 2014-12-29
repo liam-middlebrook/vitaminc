eb6f572c12fb273bd1b1fab33aff7f2b7047f7d5
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-12-28 19:09:36 -0500

    Updated complementary color test

--- test/vitaminc_tests.c
+++ test/vitaminc_tests.c
@@ -11,13 +11,9 @@ int clean_color_tests(void){
 
 void test_complementary_color(void){
     vc_color red = vc_create_color(1.0, 0.0, 0.0, VC_RGB, 1.0);
-
-    printf("Red: %f %f %f %f", red.r, red.g, red.b, red.a);
-    vc_color yellow = vc_complementary_color(red, VC_WHEEL_RYB);
-    printf("Yellow: %f %f %f %f", yellow.r, yellow.g, yellow.b, yellow.a);
-    vc_color red2 = vc_complementary_color(yellow, VC_WHEEL_RYB);
-    printf("Red2: %f %f %f %f", red2.r, red2.g, red2.b, red2.a);    
-    CU_ASSERT(vc_color_equals(red, red2));
+    vc_color red_comp = vc_create_color(0.0, 1.0, 0.5, VC_HSL, 1.0);
+    
+    CU_ASSERT(vc_color_equals(vc_complementary_color(red, VC_WHEEL_RGB), red2));
 }
 
 int main(){
