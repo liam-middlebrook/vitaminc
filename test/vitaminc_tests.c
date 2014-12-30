095028525e3867df5fcb5a83ba3d491166bb90ce
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-12-30 17:38:05 -0500

    added second print statment

--- test/vitaminc_tests.c
+++ test/vitaminc_tests.c
@@ -20,6 +20,7 @@ void test_complementary_color(void){
     CU_ASSERT(vc_color_equals(vc_complementary_color(red, VC_WHEEL_RGB), red_comp));
 
     vc_print(red_comp);
+    vc_print(vc_complementary_color(red, VC_WHEEL_RGB), red_comp);
 }
 
 int main(){
