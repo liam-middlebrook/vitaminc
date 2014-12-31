fd748fd2201039083ae6eaa8aeb1931eba29bbfd
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-12-31 01:14:26 -0500

    test now returns number of failures

--- test/vitaminc_tests.c
+++ test/vitaminc_tests.c
@@ -37,5 +37,5 @@ int main(){
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
-    return 0;//CU_get_error();
+    return CU_get_number_of_failures();
 }
