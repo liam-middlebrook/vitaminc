77f82d9dfb8a8e25ceb592069531083a38827376
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-12-31 03:49:06 -0500

    now should return whether or not all tests passed

--- test/vitaminc_tests.c
+++ test/vitaminc_tests.c
@@ -45,7 +45,7 @@ int main(){
     }
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
-    CU_basic_run_tests();
+    int errCode = CU_basic_run_tests();
     CU_cleanup_registry();
-    return CU_get_number_of_tests_failed();
+    return errCode;
 }
